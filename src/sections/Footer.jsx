d609821827006073735d168f554067faa9d2c30b
Riyad <Riyad.hu95@gmail.com>
2025-11-03 11:25:54 +0200

    change footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -4,17 +4,28 @@ const Footer = () => {
   return (
     <footer className="bg-[#ffffff6e]">
       <div className="h-20 flex justify-center items-center gap-10 text-primary">
-        <div>
-          <div className="font-light text-xl ">
-            فنون الهندسة و الاعمار{" "}
-            <span className="text-xs relative -top-1.5 text-secondry">©</span>
+        <div className="px-10 w-full flex items-start justify-around">
+          <div>
+            <p>092 555 5865</p>
+            <p>091 724 0771</p>
           </div>
-          <div className="text-center text-sm text-secondry">{getYear}</div>
+          <div className="text-center">
+            {" "}
+            <h1>فنون الهندسة و الاعمار</h1>
+            <h1>مدينة المرج, بجانب مطاحن المرج (السيلس)</h1>
+          </div>
+
+          <h1>fnonalhandsa@gmail.com</h1>
         </div>
-        <div>{/* <img src="icon_w.png" alt="" width={70} /> */}</div>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+
+// <div className="font-light text-xl ">
+//   فنون الهندسة و الاعمار{" "}
+//   <span className="text-xs relative -top-1.5 text-secondry">©</span>
+// </div>
+// <div className="text-center text-sm text-secondry">{getYear}</div>
